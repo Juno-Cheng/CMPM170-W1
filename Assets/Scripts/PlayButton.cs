b0840422aab173c7d69613a859e1208d3d58ed3e
Jonathan Cheng <chengjonathan2021@gmail.com>
2024-04-07 14:36:58 -0700

    Main Menu

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayButton : MonoBehaviour
+{
+    [Header("Game Objects")]
+    [SerializeField] public GameObject MainMenu;
+    [SerializeField] public GameObject Checker;
+    [SerializeField] private bool Sunday;
+    [SerializeField] private bool SinnerSundayBypass;
+    [SerializeField] private bool isPlaying;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sunday = DateTime.Now.DayOfWeek == DayOfWeek.Sunday;
+        Debug.Log("Is today Sunday? " + Sunday);
+        isPlaying = false;
+    }
+
+    public void onPlay()
+    {
+        //If being Played on Sunday
+        if (Sunday || SinnerSundayBypass)
+        {
+            isPlaying = true;
+            MainMenu.SetActive(false);
+        }
+        else
+        {
+            //Disable Menu & Enable Checker
+            MainMenu.SetActive(false);
+            Checker.SetActive(true);
+        }
+    }
+}
